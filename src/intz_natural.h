274d16f125f60a26f5a3f7ea0eaa9a65e8d1ebfd
Rafael Lorandi <coolparadox@gmail.com>
2025-03-18 19:40:42 +0100

    wip: intz_natural api

--- src/intz_natural.h
+++ src/intz_natural.h
@@ -2,19 +2,26 @@
 #define _INTZ_NATURAL_H_
 
 struct intz_natural;
-struct intz_natural *intz_natural_alloc(uint64_t value);
-void intz_natural_free(struct intz_natural *value);
-void intz_natural_gc(struct intz_natural *value);
-void intz_natural_reset(struct intz_natural *value);
-int intz_natural_is_zero(struct intz_natural *value);
-void intz_natural_increment(struct intz_natural *value);
-void intz_natural_add64(struct intz_natural *target, uint64_t value);
-void intz_natural_add(struct intz_natural *target, struct intz_natural *value);
-void intz_natural_mul64(struct intz_natural *target, uint64_t value);
-void intz_natural_mul(struct intz_natural *target, struct intz_natural *value);
-void intz_natural_sub64(struct intz_natural *target, uint64_t value, uint64_t *borrow);
-void intz_natural_sub(struct intz_natural *target, const struct intz_natural *value, struct intz_natural **borrow);
-void intz_natural_div64(struct intz_natural *target, uint64_t value, uint64_t *remainder);
-void intz_natural_div(struct intz_natural *target, const struct intz_natural *value, struct intz_natural **remainder);
+typedef struct intz_natural izn;
+
+/**
+ * @brief Instantiates a natural number.
+ * @param value Initial value.
+ * @return New instance of the number.
+ * @see izn_free
+ */
+izn *izn_alloc(uint64_t value);
+
+void izn_free(izn *value);
+void izn_reset(izn *value);
+int izn_is_zero(const izn *value);
+void izn_add64(izn *target, uint64_t value);
+void izn_add(izn *target, const izn *value);
+void izn_mul64(izn *target, uint64_t value);
+void izn_mul(izn *target, const izn *value);
+void izn_sub64(izn *target, uint64_t value, uint64_t *borrow);
+void izn_sub(izn *target, const izn *value, izn **borrow);
+void izn_div64(izn *target, uint64_t value, uint64_t *remainder);
+void izn_div(izn *target, const izn *value, izn **remainder);
 
 #endif // _INTZ_NATURAL_H_
